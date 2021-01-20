ff74654a1821a1e007194305cd4b723e6b5327d4
aliya <aliyasuri0@gmail.com>
2021-01-19 21:36:22 -0500

    first commit

---
+++ src/Test/BasicsofTestNG1.java
@@ -0,0 +1,24 @@
+package Test;
+import org.testng.annotations.Test;
+
+// TestNG act as JavaCompiler. it executes your all test cases 
+// we are using TestNG library but still need Java 
+// simple way is to create first method first
+// main method is required 
+// we cannot remove main method 
+// test is one annotation and recognize test and follow testcase as TestNG
+
+public class BasicsofTestNG1 {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");*/
+	
+@Test
+	public void Deom()
+	{
+		System.out.println("Demo");
+		
+	}
+
+}
